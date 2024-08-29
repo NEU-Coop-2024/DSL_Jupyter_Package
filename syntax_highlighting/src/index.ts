995e76105d27d43b10c46c399d96540e69f9941c
kyang04 <kevingyang72@gmail.com>
2024-08-29 16:03:15 -0400

    dependency

--- syntax_highlighting/src/index.ts
+++ syntax_highlighting/src/index.ts
@@ -1,36 +1,11 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
-import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
-import { hypl_syntax } from 'hypl_syntax';
-import { EditorState, Extension } from '@codemirror/state';
-import { syntaxHighlighting, HighlightStyle } from '@codemirror/language';
-import {select, selectAll} from "d3";
 
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-syntax-highlight',
+  id: 'jupyter_syntax:plugin',
+  description: 'A JupyterLab extension.',
   autoStart: true,
-  requires: [INotebookTracker],
-  activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
-    tracker.widgetAdded.connect((sender, panel: NotebookPanel) => {
-      panel.content.model?.cells.changed.connect(() => {
-        panel.content.widgets.forEach((cell) => {
-          const editor = cell.editor;
-
-          if (editor) {
-            const extensions: Extension[] = [
-              //yourSyntaxHighlighting(),
-              syntaxHighlighting(HighlightStyle.default)
-            ];
-
-            const state = EditorState.create({
-              doc: editor.model.value.text,
-              extensions
-            });
-
-            editor.state = state;
-          }
-        });
-      });
-    });
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyter_syntax is activated!');
   }
 };
 
