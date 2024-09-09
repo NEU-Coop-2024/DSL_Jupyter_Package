b643d01f7bdcd21ba0332de17f8f25597b97ed63
Kevin Yang <kevingyang72@gmail.com>
2024-09-09 18:01:55 +0000

    syntax highlighter notebook

--- syntax_highlighting_new/src/index.ts
+++ syntax_highlighting_new/src/index.ts
@@ -2,6 +2,8 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { EditorState, Extension } from '@codemirror/state';
+import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
 import { Hypl } from 'hypl_syntax';
 // import {select} from 'd3';
 
@@ -12,9 +14,31 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'myextension:plugin',
   description: 'A JupyterLab extension.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [INotebookTracker],
+  activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
     console.log('JupyterLab extension myextension is activated!');
     console.log(Hypl);
+    tracker.widgetAdded.connect((sender, panel: NotebookPanel) => {
+      panel.content.model?.cells.changed.connect(() => {
+        panel.content.widgets.forEach((cell) => {
+          const editor = cell.editor;
+
+          if (editor) {
+            const extensions: Extension[] = [
+              Hypl.syntaxHighlighting,
+              Hypl.HyplLanguage
+            ];
+
+            const state = EditorState.create({
+              doc: editor.model.value.text,
+              extensions
+            });
+
+            editor.state = state;
+          }
+        });
+      });
+    });
   }
 };
 
